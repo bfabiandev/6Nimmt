bc5ec98719f2cd8593d2a26ae73df36a3a5bf5cc
Benedek Fabian <bfabiandev@gmail.com>
2017-03-11 15:53:44 +0100

    one hot coding for states added random players smarter choice of slots changed names of functions to be more pythonic added verbose parameter

--- AI.py
+++ AI.py
@@ -3,25 +3,27 @@ from keras.layers.core import Dense, Dropout, Activation
 from keras.optimizers import RMSprop
 import keras.models
 
-def loadModel():
+
+def load_model():
     try:
         model = keras.models.load_model('6Nimmt.h5')
     except OSError or ValueError:
+        print("No previously trained model found! Creating new one...")
         model = Sequential()
-        model.add(Dense(512, init='lecun_uniform', input_shape=(10,)))
+        model.add(Dense(512, init='lecun_uniform', input_shape=(456,)))
         model.add(Activation('relu'))
-    
+
         model.add(Dense(512, init='lecun_uniform'))
         model.add(Activation('relu'))
-    
+
         model.add(Dense(104, init='lecun_uniform'))
         model.add(Activation('linear'))
-    
+
         rms = RMSprop()
         model.compile(loss='mse', optimizer=rms)
-        
+
     return model
 
 
-def saveModel(model):
+def save_model(model):
     model.save('6Nimmt.h5')
--- game.py
+++ game.py
@@ -1,10 +1,12 @@
 import random
 import numpy as np
 import AI
+import plotly.plotly as py
+import matplotlib.pyplot as plt
 
-def getSizeOfPenalty(penaltyCards):
+def get_size_of_penalty(penalty_cards):
     penalty = 0
-    for card in penaltyCards:
+    for card in penalty_cards:
         if card == 55:
             penalty += 7
         elif card % 11 == 0:
@@ -25,7 +27,7 @@ class Deck:
         self.cards = list(range(1, 105))
         np.random.shuffle(self.cards)
 
-    def getRandomCard(self):
+    def get_random_card(self):
         return self.cards.pop()
 
     def __str__(self):
@@ -35,37 +37,37 @@ class Deck:
 class Player:
     cards = None
     penalty = None
+    random = None
 
-    def __init__(self):
+    def __init__(self, random=False):
         self.cards = []
         self.penalty = 0
+        self.random = random
 
-    def addCard(self, card):
+    def add_card(self, card):
         self.cards.append(card)
 
-    def removeCard(self, card):
+    def remove_card(self, card):
         if card in self.cards:
             self.cards.remove(card)
 
-    def chooseCard(self, game, board):
-        state = np.empty(shape=(0, 0))
-        for slot in board.slots:
-            state = np.append(state, [slot[0][-1], slot[1]])
+    def choose_card(self, game):
+        if not self.random:
+            state = game.get_state()
+            qvals = model.predict(state.reshape(1, 456), batch_size=1)
+            indexes = self.cards.copy()
+            indexes[:] = [x - 1 for x in indexes]
+            possible_qvals = qvals[:, indexes]
+            max_index = np.argmax(possible_qvals)
+            chosen = self.cards[max_index]
+        else:
+            chosen = np.random.choice(self.cards)
 
-        state = np.append(state, [len(game.players)])
-        state = np.append(state, [len(self.cards)])
-        qvals = model.predict(state.reshape(1, 10), batch_size=1)
-        indexes = self.cards.copy()
-        indexes[:] = [x - 1 for x in indexes]
-        possibleQvals = qvals[:, indexes]
-        maxIndex = np.argmax(possibleQvals)
-        chosen = self.cards[maxIndex]
-
-        self.removeCard(chosen)
+        self.remove_card(chosen)
         return chosen
 
-    def addToPenalty(self, penaltyCards):
-        self.penalty += getSizeOfPenalty(penaltyCards)
+    def add_to_penalty(self, penaltyCards):
+        self.penalty += get_size_of_penalty(penaltyCards)
 
     def __str__(self):
         return str(self.cards)
@@ -77,17 +79,27 @@ class Board:
     def __init__(self):
         self.slots = []
 
-    def fillSlots(self, deck):
+    def fill_slots(self, deck):
         for i in range(4):
-            self.slots.append([[deck.getRandomCard()], 1])
+            self.slots.append([[deck.get_random_card()], 1])
         self.slots.sort(key=lambda tup: tup[0][-1])
+
+    def choose_slot(self):
+        min_penalty = 1000
+        best_slot_index = -1
+        for i, slot in enumerate(self.slots):
+            penalty = get_size_of_penalty(slot[0])
+            if penalty < min_penalty:
+                min_penalty = penalty
+                best_slot_index = i
+        return best_slot_index
 
     def __str__(self):
         return str(self.slots)
 
-    def addCard(self, card):
+    def add_card(self, card):
         index = 3
-        penaltyCards = []
+        penalty_cards = []
 
         for i, slot in enumerate(self.slots):
             if slot[0][-1] > card[0]:
@@ -95,21 +107,22 @@ class Board:
                 break
 
         if index == -1:
-            index = np.random.randint(0, 4)
-            penaltyCards = self.slots[index][0]
+            # index = np.random.randint(0, 4)
+            index = self.choose_slot()
+            penalty_cards = self.slots[index][0]
             self.slots[index] = [[card[0]], 1]
             self.slots.sort(key=lambda tup: tup[0][-1])
 
         else:
             if self.slots[index][1] == 5:
-                penaltyCards = self.slots[index][0]
+                penalty_cards = self.slots[index][0]
                 self.slots[index] = [[card[0]], 1]
                 self.slots.sort(key=lambda tup: tup[0][-1])
             else:
                 self.slots[index][0].append(card[0])
                 self.slots[index][1] += 1
 
-        return penaltyCards
+        return penalty_cards
 
 
 class Game:
@@ -117,147 +130,176 @@ class Game:
     players = None
     board = None
 
-    def __init__(self, numberOfPlayers=2):
+    def __init__(self, number_of_players=2, random_players=0):
         self.deck = Deck()
         self.players = []
         self.board = Board()
-        self.dealHands(numberOfPlayers)
-        self.board.fillSlots(deck=self.deck)
+        self.deal_hands(number_of_players, random_players)
+        self.board.fill_slots(deck=self.deck)
 
-    def dealHands(self, numberOfPlayers=2):
-        if numberOfPlayers > 10 or numberOfPlayers < 1:
+    def deal_hands(self, number_of_players=2, random_players=0):
+        if number_of_players > 10 or number_of_players < 1:
             print("Not a valid number of players. [2-10]")
 
-        for i in range(numberOfPlayers):
-            self.players.append(Player())
+        for i in range(number_of_players-random_players):
+            self.players.append(Player(random=False))
+        for i in range(random_players):
+            self.players.append(Player(random=True))
 
         for hand in self.players[:]:
             for i in range(10):
-                hand.addCard(self.deck.getRandomCard())
+                hand.add_card(self.deck.get_random_card())
             hand.cards.sort()
 
-    def printHands(self):
+    def print_hands(self):
         for i, hand in enumerate(self.players): print("Player {}'s hand: {}".format(i, hand))
 
-    def printDeck(self):
+    def print_deck(self):
         print("Deck: " + str(self.deck))
 
-    def printBoard(self):
+    def print_board(self):
         print("Board: " + str(self.board))
 
-    def playGame(self, history=None):
+    def play_game(self, history=None, verbose=False):
         if history is None:
             history = []
 
         for i in range(10):
-            self.playRound(history)
-            self.printHands()
-            self.printBoard()
+            self.play_round(history, verbose)
+            if verbose:
+                self.print_hands()
+                self.print_board()
 
-    def playRound(self, history=None):
+    def play_round(self, history=None, verbose=False):
         if history is None:
             history = []
 
-        oldState = self.getState()
-        oldCards = []
+        old_state = self.get_state()
+        old_cards = []
         for i in self.players:
-            oldCards.append(i.cards.copy())
+            old_cards.append(i.cards.copy())
 
-        cardsThisTurn = []
+        cards_this_turn = []
         for i, hand in enumerate(self.players):
-            chosenCard = hand.chooseCard(self, self.board)
-            cardsThisTurn.append((chosenCard, i))
-            print("{}. player pick card {}.".format(i, chosenCard))
+            chosen_card = hand.choose_card(self)
+            cards_this_turn.append((chosen_card, i))
+            if verbose:
+                print("{}. player pick card {}.".format(i, chosen_card))
 
-        penalties = self.putCardsOnBoard(cardsThisTurn)
+        penalties = self.put_cards_on_board(cards_this_turn, verbose)
         for penalty in penalties:
-            for card in cardsThisTurn:
+            for card in cards_this_turn:
                 if penalty[1] == card[1]:
-                    newCards = self.players[card[1]].cards.copy()
-                    history.append((oldState, oldCards[card[1]], card[0], -penalty[0], self.getState(), newCards))
+                    new_cards = self.players[card[1]].cards.copy()
+                    history.append((old_state, old_cards[card[1]], card[0], -penalty[0], self.get_state(), new_cards))
 
-    def putCardsOnBoard(self, cards):
+    def put_cards_on_board(self, cards, verbose=False):
         penalties = []
         cards.sort(key=lambda tup: tup[0])
         for card in cards:
-            penaltyCards = self.board.addCard(card)
-            self.players[card[1]].addToPenalty(penaltyCards)
-            penalties.append((getSizeOfPenalty(penaltyCards), card[1]))
+            penalty_cards = self.board.add_card(card)
+            self.players[card[1]].add_to_penalty(penalty_cards)
+            penalties.append((get_size_of_penalty(penalty_cards), card[1]))
 
-            if len(penaltyCards) > 0:
-                print("{}. Player's penalty increased by {}.".format(card[1], getSizeOfPenalty(penaltyCards)))
+            if len(penalty_cards) > 0 and verbose:
+                print("{}. Player's penalty increased by {}.".format(card[1], get_size_of_penalty(penalty_cards)))
 
         return penalties
 
-    def getState(self):
+    def get_state(self):
         state = np.empty(shape=(0, 0))
         for slot in self.board.slots:
-            state = np.append(state, [slot[0][-1], slot[1]])
+            state = np.append(state, state_to_one_hot(slot[0][-1], 104))
+            state = np.append(state, state_to_one_hot(slot[1], 5))
 
-        state = np.append(state, [len(self.players)])
-        state = np.append(state, [len(self.players[0].cards)])
+        state = np.append(state, state_to_one_hot(len(self.players), 10))
+        state = np.append(state, state_to_one_hot(len(self.players[0].cards), 10))
         return state
 
-    def printPenalties(self):
+    def print_penalties(self):
         for i, player in enumerate(self.players):
             print("{}. Player's penalty: {}".format(i, player.penalty))
 
+    def get_penalties(self):
+        penalties = np.zeros(len(self.players))
+        for i, player in enumerate(self.players):
+            penalties[i] = player.penalty
+        return penalties
+
+def state_to_one_hot(number, num_categories):
+    arr = np.zeros(shape=(num_categories))
+    arr[number - 1] = 1
+    return arr
+
 
 def train():
-    epochs = 5000
+    epochs = 2000
     gamma = 0.25
-    historySize = 500
+    history_size = 5000
     history = []
 
     for i in range(epochs):
-        batchSize = 100
+        batch_size = 500
         game = Game(np.random.randint(2, 11))
-        game.playGame(history)
-        if len(history) > historySize:
-            history = history[-historySize:]
-        if batchSize > len(history):
-            batchSize = len(history)
-        minibatch = random.sample(history, batchSize)
-        X_train = []
-        Y_train = []
+        game.play_game(history)
+        if len(history) > history_size:
+            history = history[-history_size:]
+        if batch_size > len(history):
+            batch_size = len(history)
+        minibatch = random.sample(history, batch_size)
+        x_train = []
+        y_train = []
         for memory in minibatch:
-            oldState, oldCards, action, reward, newState, newCards = memory
-            oldQvals = model.predict(oldState.reshape(1,10), batch_size=1)
+            old_state, old_cards, action, reward, new_state, new_cards = memory
+            old_qvals = model.predict(old_state.reshape(1, 456), batch_size=1)
 
-            y = np.zeros((1,104))
-            y[:] = oldQvals[:]
+            y = np.zeros((1, 104))
+            y[:] = old_qvals[:]
 
-            if len(newCards)==0:
+            if len(new_cards) == 0:
                 update = reward
             else:
-                newQvals = model.predict(newState.reshape(1, 10), batch_size=1)
+                new_qvals = model.predict(new_state.reshape(1, 456), batch_size=1)
 
-                indexes = newCards.copy()
+                indexes = new_cards.copy()
                 indexes[:] = [x - 1 for x in indexes]
-                possibleQvals = newQvals[:, indexes]
-                maxNewQval = np.max(possibleQvals)
-                update = reward + gamma*maxNewQval
+                possible_qvals = new_qvals[:, indexes]
+                max_new_qval = np.max(possible_qvals)
+                update = reward + gamma * max_new_qval
 
-            y[0][action-1] = update
-            X_train.append(oldState.reshape(10,))
-            Y_train.append(y.reshape(104,))
+            y[0][action - 1] = update
+            x_train.append(old_state.reshape(456, ))
+            y_train.append(y.reshape(104, ))
 
-
-        X_train = np.array(X_train)
-        Y_train = np.array(Y_train)
+        x_train = np.array(x_train)
+        y_train = np.array(y_train)
 
         print("Game #: %s" % (i,))
-        model.fit(X_train, Y_train, batch_size=batchSize, nb_epoch=1, verbose=1)
+        model.fit(x_train, y_train, batch_size=batch_size, nb_epoch=1, verbose=1)
 
 
-model = AI.loadModel()
+model = AI.load_model()
 
 #train()
 
-AI.saveModel(model)
+AI.save_model(model)
 
-game = Game(4)
-game.printHands()
-game.printBoard()
-game.playGame()
-game.printPenalties()
+number_of_games = 10000
+statistics = np.empty(shape=(number_of_games,10), dtype=int)
+for i in range(number_of_games):
+    print("Game #: {}".format(i))
+    game = Game(10, random_players=8)
+    game.play_game(verbose=False)
+    pens = game.get_penalties()
+    statistics[i, :] = pens
+
+mean = np.mean(statistics, axis=0)
+variance = np.var(statistics, axis=0)
+
+for i in range(len(mean)):
+    print("Player {}'s scores have a mean of {} and variance of {}.".format(i, mean[i], variance[i]))
+#fig = plt.figure()
+
+#n, bins, patches = plt.hist(mean)
+
+#plt.show()
